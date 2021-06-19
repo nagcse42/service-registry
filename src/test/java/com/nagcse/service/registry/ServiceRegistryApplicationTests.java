046bdf77e87058b1f2050e9044d4968ba798edd0
nagcse42 <nagcse42@gmail.com>
2021-06-19 14:55:45 +0400

    Eureka Service Registry for Flight and Passenger changes committed.

---
+++ src/test/java/com/nagcse/service/registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nagcse.service.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
